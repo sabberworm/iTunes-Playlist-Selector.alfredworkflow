d3b8c402ec528afa3c0dbc28ef1100a508e39a05
Raphael Schweikert <any@sabberworm.com>
2016-08-29 08:37:55 +0200

    Add JXA-based extract playlists script that includes types
    (to later be used for icons and filtering)

---
+++ read_playlists.js
@@ -0,0 +1,81 @@
+#!/usr/bin/osascript -l JavaScript
+
+const FLAGS = {
+	smart: 's',
+	library: 'l',
+	folder: 'f',
+	subscription: 'S',
+	user: 'u',
+	genius: 'g',
+	radio: 'r',
+	cd: 'c'
+};
+
+var app = Application('iTunes');
+app.includeStandardAdditions = true;
+
+var playlists = {};
+
+app.userPlaylists().forEach(function(playlist) {
+	playlists[playlist.persistentID()] = {
+		name: playlist.name(),
+		user: true,
+		smart: playlist.smart(),
+		genius: playlist.genius(),
+		shared: playlist.shared()
+	};
+});
+
+app.libraryPlaylists().forEach(function(playlist) {
+	playlists[playlist.persistentID()] = {
+		name: playlist.name(),
+		library: true
+	};
+});
+
+app.radioTunerPlaylists().forEach(function(playlist) {
+	playlists[playlist.persistentID()] = {
+		name: playlist.name(),
+		radio: true
+	};
+});
+
+app.subscriptionPlaylists().forEach(function(playlist) {
+	playlists[playlist.persistentID()] = {
+		name: playlist.name(),
+		subscription: true
+	};
+});
+
+app.audioCDPlaylists().forEach(function(playlist) {
+	playlists[playlist.persistentID()] = {
+		name: playlist.name(),
+		cd: true
+	};
+});
+
+app.playlists().forEach(function(playlist) {
+	var id = playlist.persistentID();
+	if(id in playlists) {
+		return;
+	}
+	// We have gone through all other types of playlists, only folders left
+	playlists[id] = {
+		name: playlist.name(),
+		folder: true
+	};
+});
+
+playlists = Object.keys(playlists).filter(function(id) {
+	return !playlists[id].cd;
+}).map(function(id) {
+	var flags = '';
+	for(var f in FLAGS) {
+		if(playlists[id][f]) {
+			flags += FLAGS[f];
+		}
+	}
+	return `${id}:${flags}:${playlists[id].name}`;
+})
+
+console.log(playlists.join('\n'));
